c7d652a37c0bcdcabd57b804a66621de848b5489
Jakob Offersen <jakoboffersen@gmail.com>
2020-02-21 15:23:54 +0100

    add toplevel interface

---
+++ main.js
@@ -0,0 +1,5 @@
+const toplevel = require('./toplevel')
+
+toplevel.invite("disc words", "B", (err, success) => {
+    console.log(err, success)
+})
---
+++ toplevel.js
@@ -0,0 +1,22 @@
+function invite(discoveryWords, name, cb) {
+    cb(null, true)
+}
+
+function sendMessageTo(name, message) {
+
+}
+
+function join() {
+
+}
+
+function leave() {
+
+}
+
+module.exports = {
+    invite: invite,
+    sendMessageTo: sendMessageTo,
+    join: join,
+    leave: leave
+}
