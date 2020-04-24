1927b14d79c14bb890adedf095b93cb143250a8a
Jakob Offersen <jakoboffersen@gmail.com>
2020-04-24 14:52:49 +0200

    WIP

--- vectorClock.js
+++ vectorClock.js
@@ -2,6 +2,30 @@ const LEQ = 1
 const GEQ = 2
 const PAR = 3
 
+class VectorChunk {
+
+    constructor(vectors) {
+        this._chunk = vectors
+        this._orderChunk()
+    }
+
+    newest() {
+        return this._chunk[0]
+    }
+
+    oldest() {
+        return this._chunk[this._chunk.length - 1]
+    }
+
+    // Note: Ensures that we always have decreasing vectors
+    _orderChunk() {
+        // Empty or 1-elem chunks are implicitly ordered
+        if (this._chunk.length < 2) return
+        // v1 < v2 => reverse
+        if (_compare(this._chunk[0], this._chunk[1]) === LEQ) this._chunk = this._chunk.reverse()
+    }
+}
+
 function _compare(v1, v2) {
     var leq = 0
     var geq = 0
@@ -50,6 +74,27 @@ function computeMax(vs) {
     return res
 }
 
+// CASE 1 is assumed
+function case1(chunk1, chunk2) {
+    return {
+        left: range(0, chunk1.length),
+        right: [],
+        rest: range(0, chunk2.length)
+    }
+}
+
+// CASE 2 is assumed
+function case2(chunk1, chunk2) {
+    let reference = chunk1.oldest()
+    let { before, after } = splitChunkUsingReference(reference, chunk2)
+
+    return {
+        left: range(0, chunk1.length),
+        right: after,
+        rest: before
+    }
+}
+
 // CASE 2 is assumed
 // Split c2 into {before: [indices], after: [indices]} based on 'reference' vector.
 function splitChunkUsingReference(reference, c2) {
@@ -60,6 +105,7 @@ function splitChunkUsingReference(reference, c2) {
         after: range(0, splitIndex)
     }
 }
+
 
 // Works like pythons 'range' with step = 1
 function range(start, stop) {
