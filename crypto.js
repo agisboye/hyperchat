a6ca87876634904bee82a7fcab23afbe57cf9bf8
agisboye <august@heegaard.org>
2020-04-24 14:39:07 +0200

    Generate symmetric key from random bytes

--- crypto.js
+++ crypto.js
@@ -1,13 +1,5 @@
 const sodium = require('sodium-native')
 
-/// returns (rx, tx)
-function _generateClientKeys(clientPublicKey, clientSecretKey, serverPublicKey) {
-    let rx = Buffer.alloc(sodium.crypto_kx_SESSIONKEYBYTES)
-    let tx = Buffer.alloc(sodium.crypto_kx_SESSIONKEYBYTES)
-    sodium.crypto_kx_client_session_keys(rx, tx, clientPublicKey, clientSecretKey, serverPublicKey)
-    return { rx, tx }
-}
-
 function splitNonceAndCipher(cipherAndNonce) {
     // nonce is always prepended to cipher when encrypted. 
     // Therefore, copy the first part into 'nonce' and second part into 'ciphertext'
@@ -19,21 +11,16 @@ function splitNonceAndCipher(cipherAndNonce) {
     return { nonce, cipher }
 }
 
-// ------- public functions ------- //
-
-/// Returns (pk, sk)
-function generateKeyPair() {
-    let pk = Buffer.alloc(sodium.crypto_kx_PUBLICKEYBYTES)
-    let sk = Buffer.alloc(sodium.crypto_kx_SECRETKEYBYTES)
-    sodium.crypto_kx_keypair(pk, sk)
-    return { pk, sk }
+function generateNonce() {
+    let nonce = Buffer.alloc(sodium.crypto_secretbox_NONCEBYTES)
+    sodium.randombytes_buf(nonce)
+    return nonce
 }
 
 function generateSymmetricKey() {
-    let clientKeys = generateKeyPair()
-    let serverKeys = generateKeyPair()
-    let { rx, tx } = _generateClientKeys(clientKeys.pk, clientKeys.sk, serverKeys.pk)
-    return rx
+    let nonce = Buffer.alloc(sodium.crypto_secretbox_KEYBYTES)
+    sodium.randombytes_buf(nonce)
+    return nonce
 }
 
 function encryptMessage(plainMessage, key) {
@@ -63,12 +50,6 @@ function makeChatID(key, senderFeedKey) {
     let output = Buffer.alloc(sodium.crypto_generichash_BYTES_MAX)
     sodium.crypto_generichash(output, Buffer.concat([key, senderFeedKey]))
     return output
-}
-
-function generateNonce() {
-    let nonce = Buffer.alloc(sodium.crypto_secretbox_NONCEBYTES)
-    sodium.randombytes_buf(nonce)
-    return nonce
 }
 
 function hash(input) {
